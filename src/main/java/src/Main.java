f508d0ed90947a577da07bf10088c5c58dd57c9b
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-05-06 10:41:38 +0200

    extracted methods from main

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,10 +1,26 @@
 package src;
 
 public class Main {
+    private static int milesPerGallon = 20;
+    private static double gallonsOfGas = 10.0;
+    private static int odometer = 0;
+
     public static void main(String[] args) {
         System.out.println("In vehicles Main");
         System.out.println("Second line");
-        System.out.println("Let see what this does");
-        System.out.println("Now its kinda working");
+
+
+        System.out.println("Odometer " + odometer + " Gallons of gas " + gallonsOfGas);
+
+        drive(100);
+
+        drive(60);
+        drive(80);
+    }
+
+    private static void drive(int milesDriven) {
+        gallonsOfGas -= (milesDriven / milesPerGallon);
+        odometer += milesDriven;
+        System.out.println("Odometer " + odometer + " Gallons of gas " + gallonsOfGas);
     }
 }
