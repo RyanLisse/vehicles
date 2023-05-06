b77222db17975625e146ff189087e7189947dd9c
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-05-06 10:58:23 +0200

    Added paralel arrays

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,26 +1,36 @@
 package src;
 
+import java.util.Arrays;
+
 public class Main {
-    private static int milesPerGallon = 20;
-    private static double gallonsOfGas = 10.0;
-    private static int odometer = 0;
+//    private static int milesPerGallon = 20;
+//    private static double gallonsOfGas = 10.0;
+//    private static int odometer = 0;
+
+
+    private static int[] milesPerGallon = {20, 25, 15};
+    private static double[] gallonsOfGas = {10.0, 8, 12};
+    private static int[] odometer = {0, 10_000, 20_000};
 
     public static void main(String[] args) {
         System.out.println("In vehicles Main");
         System.out.println("Second line");
 
 
-        System.out.println("Odometer " + odometer + " Gallons of gas " + gallonsOfGas);
-
         drive(100);
 
         drive(60);
-        drive(80);
+
     }
 
     private static void drive(int milesDriven) {
-        gallonsOfGas -= (milesDriven / milesPerGallon);
-        odometer += milesDriven;
-        System.out.println("Odometer " + odometer + " Gallons of gas " + gallonsOfGas);
+        for (int i = 0; i < 3; i++) {
+
+
+            System.out.println(i + " Odometer " + odometer[i] + " Gallons of gas " + gallonsOfGas[i]);
+            gallonsOfGas[i] -= (milesDriven / milesPerGallon[i]);
+            odometer[i] += milesDriven;
+            System.out.println(i + " Odometer " + odometer[i] + " Gallons of gas " + gallonsOfGas[i]);
+        }
     }
 }
